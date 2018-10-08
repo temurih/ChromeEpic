91f153dc1a588b6ac64412a9a2c4c44e95abd953
Hafiz Temuri <hafiz@zept.ca>
2018-10-07 21:01:26 -0600

    disable target _blank and empty links

--- src/content/contentScript.js
+++ src/content/contentScript.js
@@ -1,1 +1,12 @@
-console.log("Running Content Script...");
+
+chrome.runtime.onMessage.addListener(msg => {
+    if (!msg.target) {
+        return;
+    }
+
+    for (const elem of document.querySelectorAll("a")) {
+        if (elem.target === "_blank" || !elem.hasChildNodes()) {
+            elem.remove();
+        }
+    }
+});
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,14 +2,15 @@
 var button = document.getElementById("run");
 
 button.onclick = () => {
-    var val = {
-        "js": document.getElementById("js").checked,
-        "jsmap": document.getElementById("jsmap").checked,
-        "dts": document.getElementById("dts").checked,
-        "cs": document.getElementById("cs").checked,
-        "target": document.getElementById("tar").checked,
-    };
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        var val = {
+            "js": document.getElementById("js").checked,
+            "jsmap": document.getElementById("jsmap").checked,
+            "dts": document.getElementById("dts").checked,
+            "cs": document.getElementById("cs").checked,
+            "target": document.getElementById("tar").checked,
+            "tab": tabs[0] // active tab
+        };
         chrome.tabs.sendMessage(tabs[0].id, val);
     });
 };
